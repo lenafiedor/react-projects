ea1e7d01c862f51a31a3a8767764b14d57b129e3
Lena Fiedor <lfiedor@splunk.com>
2024-08-12 15:20:50 +0200

    Complete application

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,27 +1,61 @@
-import { CORE_CONCEPTS } from './data.js';
+import {useState} from "react";
+
+import {CORE_CONCEPTS, EXAMPLES} from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    function handleClick(selectedButton) {
+        setSelectedTopic(selectedButton);
+    }
+
     return (
         <div>
-            <Header />
+            <Header/>
             <main>
                 <section id='core-concepts'>
                     <h2>Core Concepts</h2>
                     <ul>
-                        <CoreConcept{...CORE_CONCEPTS[0]}/>
-                        <CoreConcept{...CORE_CONCEPTS[1]}/>
-                        <CoreConcept{...CORE_CONCEPTS[2]}/>
-                        <CoreConcept{...CORE_CONCEPTS[3]}/>
+                        {CORE_CONCEPTS.map((conceptItem) => (
+                            <CoreConcept key={conceptItem.title} {...conceptItem} />
+                        ))}
                     </ul>
                 </section>
                 <section id='examples'>
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton>Components</TabButton>
+                        <TabButton
+                            isActive={selectedTopic === 'components'}
+                                   onClick={() => handleClick('components')}>Components
+                        </TabButton>
+                        <TabButton
+                            isActive={selectedTopic === 'jsx'}
+                                   onClick={() => handleClick('jsx')}>JSX
+                        </TabButton>
+                        <TabButton
+                            isActive={selectedTopic === 'props'}
+                                   onClick={() => handleClick('props')}>Props
+                        </TabButton>
+                        <TabButton
+                            isActive={selectedTopic === 'state'}
+                                   onClick={() => handleClick('state')}>State
+                        </TabButton>
                     </menu>
+                    {!selectedTopic ? (
+                        <p>Please select a topic.</p>
+                    ) : (
+                        <div id='tab-content'>
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description}</p>
+                            <pre>
+                                <code>{EXAMPLES[selectedTopic].code}</code>
+                            </pre>
+                        </div>
+                    )}
                 </section>
             </main>
         </div>
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,3 +1,6 @@
-export default function TabButton(props) {
-    return <li><button>{props.children}</button></li>
+export default function TabButton({ children, onClick, isActive }) {
+
+    return <li>
+        <button className={ isActive ? 'active' : ''} onClick={onClick}>{children}</button>
+    </li>
 }
