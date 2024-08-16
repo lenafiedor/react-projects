e416592ce10e50cd09768f6e0a7f193521d3e4bc
Lena Fiedor <lfiedor@splunk.com>
2024-08-16 14:25:21 +0200

    Final changes & .gitignore

--- 01-starting-project/src/components/TabButton.jsx
+++
@@ -1,6 +0,0 @@
-export default function TabButton({ children, onClick, isActive }) {
-
-    return <li>
-        <button className={ isActive ? 'active' : ''} onClick={onClick}>{children}</button>
-    </li>
-}
